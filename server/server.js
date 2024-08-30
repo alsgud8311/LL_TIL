1e603c7d54f90d851354e6e492e40a1e6c36cf14
Minhyung Cho <dup33333@hufs.ac.kr>
2024-08-31 02:53:40 +0900

    setting: server

--- server/server.js
+++ server/server.js
@@ -1,20 +1,54 @@
 import express from 'express';
 import dotenv from 'dotenv';
+import { fileURLToPath } from 'url';
+import { dirname, join } from 'path';
+import next from 'next';
+import { parse } from 'url';
 
-dotenv.config({ path: '.env' }); // 환경변수 사용
+dotenv.config({ path: '.env' });
 
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
+/** Create Express */
 const app = express();
-const port = process.env.PORT; //.env 파일에서 설정해준다
 
-app.set('port', port);
+/** Next.js 설정 */
+const port = process.env.SERVER_PORT;
+/**
+ * 개발환경이아니라면 dev 옵션을 false 로 설정하고
+ * 서버 시작전에 next build 를 실행해준다.
+ */
+const nextApp = next({ dev: true, port });
+const handle = nextApp.getRequestHandler();
+
+await nextApp.prepare();
+
+/** Express Settings */
 app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
+app.use(express.urlencoded({ extended: true }));
+/** static 경로 설정 */
+app.use(express.static(join(__dirname, '../', 'public')));
 
-app.use('/', (req, res, next) => {
+/** Express Router Settings */
+app.use('/api', (req, res, next) => {
   res.send('hello!');
 });
 
-app.listen(app.get('port'), () => {
+/** Next.js Routing */
+app.get('/', (req, res) => {
+  const parsedUrl = parse(req.url, true);
+  const { pathname, query } = parsedUrl;
+  nextApp.render(req, res, pathname, query);
+});
+
+app.get('*', (req, res) => {
+  return handle(req, res);
+});
+
+app.listen(port, () => {
   console.log(`Express server listen port:${port}`);
   console.log(`http://localhost:${port}`);
 });
+
+export default app;
